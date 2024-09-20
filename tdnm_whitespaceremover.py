4ecf48623303f4c4f762cd610cd0071ebbd00bdc
Oliver Mustoe <polivermustoe@gmail.com>
2024-09-20 22:27:51 +0000

    cleaning up

--- tdnm_whitespaceremover.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-def fileGenerator(nixString):
-    newString = nixString.read().replace('\n','')
-
-
-with open("configuration.nix", 'r') as file:
-    newString = file.read().replace('\n','')
-    print(newString)
