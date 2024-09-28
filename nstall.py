35e4406ab6a4bde04dbb5b36acbc2353a473ec58
Oliver Mustoe <polivermustoe@gmail.com>
2024-09-28 21:36:51 +0000

    can now add to a configuration.toml - need to add removing now

--- nstall.py
+++ nstall.py
@@ -1,57 +1,59 @@
 #!/usr/bin/env python3
 
 import sys
+import subprocess
+import os
+import toml
 
-file_path = "/etc/nixos/configuration.nix"
+# Replace later with actual way to select a directory and a file (or at least a directory!)
+dir_path = "/etc/nixos"
+file_path = f"{dir_path}/configuration.nix"
+
 
 def add_package_to_nix_config(package_name):
+    defaultToml = {"packages": {"nixpkgs": {"pks": []}}}
+    tomlFile = f"{dir_path}/configuration.toml"
+    if not os.path.isfile(tomlFile):
+        with open(tomlFile, "w") as f:
+            toml.dump(defaultToml, f)
     try:
         # Read the file content
-        with open(file_path, "r") as file:
-            lines = file.readlines()
-        
-        # Find the line and insert the package after it
-        for i, line in enumerate(lines):
-            if 'environment.systemPackages = with pkgs; [' in line:
-                lines.insert(i + 1, f"  {package_name} # added by nstall\n")
-                break
-        
-        # Write the modified content back to the file
-        with open(file_path, "w") as file:
-            file.writelines(lines)
-        
-        print(f"Successfully added '{package_name}' to {file_path}")
-    
+        with open(f"{dir_path}/configuration.toml", "r") as file:
+            config = toml.load(file)
+            config['packages']['nixpkgs']['pks'].append(package_name)
+        with open(f"{dir_path}/configuration.toml", "w") as file:
+            toml.dump(config, file)
     except Exception as e:
         print(f"Error: {e}")
 
+
 def remove_package_from_nix_config(package_name):
     in_system_packages_block = False
-
     try:
         # Read the file content
-        with open(file_path, "r") as file:
+        with open(f"{dir_path}/configuration.toml", "r") as file:
             lines = file.readlines()
-        
+
         # Filter out the specified package between the systemPackages block
         for i, line in enumerate(lines):
-            if 'environment.systemPackages = with pkgs; [' in line:
+            if "environment.systemPackages = with pkgs; [" in line:
                 in_system_packages_block = True  # Start tracking block
                 continue
-            elif '];' in line and in_system_packages_block:
+            elif "];" in line and in_system_packages_block:
                 in_system_packages_block = False  # End of block, stop tracking
                 break
             elif in_system_packages_block:
                 if package_name in line:
                     print(f"Removing '{package_name}' from {file_path}")
-                    lines[i] = ''  # Remove the line containing the package
-        
+                    lines[i] = ""  # Remove the line containing the package
+
         # Write the modified content back to the file
         with open(file_path, "w") as file:
             file.writelines(lines)
-        
+
     except Exception as e:
         print(f"Error: {e}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -59,10 +61,27 @@ if __name__ == "__main__":
     else:
         action = sys.argv[1].lower()
         package_name = sys.argv[2]
+        rebuild = False
 
         if action == "install" or action == "add":
-            add_package_to_nix_config(package_name)
+            try:
+                add_package_to_nix_config(package_name)
+                rebuild = True
+            except Exception as e:
+                print(e)
+            # Run a nixos-rebuild
         elif action == "remove":
-            remove_package_from_nix_config(package_name)
+            try:
+                remove_package_from_nix_config(package_name)
+                rebuild = True
+            except Exception as e:
+                print(e)
         else:
             print("nstall: Invalid action. Use 'add' or 'remove'.")
+
+#        if rebuild:
+#            rebuildOutput = ["nixos-rebuild", "switch",
+#                            "-I", "nixos-config=", file_path]
+#           rebuildOutput = subprocess.check_output(
+#               args=rebuildOutput, shell=False)
+#            print(rebuildOutput)
--- shell.nix
+++ shell.nix
@@ -2,4 +2,4 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/dbebdd67a6006bb145d98c8debf9140ac7e651d0.tar.gz";
   pkgs = import nixpkgs { };
 in
-pkgs.mkShellNoCC { packages = with pkgs; [ (python3.withPackages (ps: with ps; [ toml ])) ]; }
+pkgs.mkShellNoCC { packages = with pkgs; [ (python3.withPackages (ps: with ps; [ toml ])) black ]; }
